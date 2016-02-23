9ad243f312f7e72b800375bee509b5bbcd150c2d
tonispi <tonis.pihlakas@playtech.com>
2016-02-23 20:00:51 +0200

    Added missing files

---
+++ spock-rest-example/src/main/java/eu/t6nn/tutorial/spock/rest/SpockRestExampleApplication.java
@@ -0,0 +1,12 @@
+package eu.t6nn.tutorial.spock.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpockRestExampleApplication.class, args);
+	}
+}
